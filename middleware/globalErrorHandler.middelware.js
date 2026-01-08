5e9f0e7f8f2922136002402abc08a5cbc07cefd3
adirpathak7 <adityarpathak7@gmail.com>
2026-01-08 12:42:11 +0530

    bug fixed

--- middleware/globalErrorHandler.middelware.js
+++ middleware/globalErrorHandler.middelware.js
@@ -1,7 +1,4 @@
 module.exports = (err, req, res, next) => {
-  // console.log("err: ", err);
-  // console.log("err.Joi: ", err.isJoi);
-
   if (err.isJoi) {
     return res.status(400).json({
       success: false,
@@ -10,58 +7,14 @@ module.exports = (err, req, res, next) => {
     });
   }
 
-  if (err.isJoi) {
-    return res.status(422).json({
-      success: false,
-      message: "Unprocessable Entity!",
-      errors: err.details.map((e) => e.message),
-    });
-  }
-
-  if (err.isJoi) {
-    return res.status(404).json({
-      success: false,
-      message: "No data found!",
-      errors: err.details.map((e) => e.message),
-    });
-  }
-
-  if (err.isJoi) {
-    return res.status(401).json({
-      success: false,
-      message: "Unauthorize:!",
-      errors: err.details.map((e) => e.message),
-    });
-  }
-
-  if (err.isJoi) {
-    return res.status(403).json({
-      success: false,
-      message: "Forbidden: Access Denied!",
-      errors: err.details.map((e) => e.message),
-    });
-  }
-
-  if (err.isJoi) {
-    return res.status(503).json({
-      success: false,
-      message: "Service temporarily unavailable!",
-      errors: err.details.map((e) => e.message),
-    });
-  }
-
   if (err.status) {
-    // console.log("err.status in globleErrorHandle:- ", err.status);
-    // console.log("err in globleErrorHandle:- ", err);
-    // console.log("err in globleErrorHandle:- ", err.message);
-
     return res.status(err.status).json({
       success: false,
       message: err.message,
     });
   }
 
-  console.error("Unknown error status:- ", err.status);
+  console.error("Unknown error status code:- ", err.status);
   console.error("Unknown error:- ", err);
 
   res.status(500).json({
